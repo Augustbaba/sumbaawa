ec8ae08d73fc885d6d8f2c7c380a136bb38b0cc0
Adebissi <adebissiimorou@gmail.com>
2025-08-20 16:03:35 +0200

    favicon

--- resources/views/front/layouts/partials/start.blade.php
+++ resources/views/front/layouts/partials/start.blade.php
@@ -10,8 +10,10 @@
     <meta name="keywords" content="multikart">
     <meta name="author" content="multikart">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/images/favicon.png') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset(FrontHelper::getEnvFolder() . 'storage/front/assets/images/favicon.png') }}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(FrontHelper::getEnvFolder() . 'favicon_io/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(FrontHelper::getEnvFolder() . 'favicon_io/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset(FrontHelper::getEnvFolder() . 'favicon_io/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset(FrontHelper::getEnvFolder() . 'favicon_io/site.webmanifest') }}">
     <title>{{ FrontHelper::getAppName() }} - @yield('title')</title>
 
     <!--Google font-->
